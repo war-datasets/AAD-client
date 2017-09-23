48ef499e4da3eac56cee3bacbe9c8d76ee115538
Tjoosten <Tjoosten3@gmail.com>
2017-09-24 01:19:18 +0200

    Implement database tables seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,11 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Permissions::class, function (Faker $faker) {
+    return ['name' => $faker->name];
+});
+
+$factory->define(App\Roles::class, function (Faker $faker) {
+    return ['name' => $faker->name];
+});
