9bcf5022c1c31249d8815da68c4727136aa77ee7
Tjoosten <Tjoosten3@gmail.com>
2017-10-15 18:32:19 +0200

    Translate casualty index

---
+++ resources/lang/en/casualties.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Casualties Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by casualties views.
+    |
+    */
+
+    // Index - table
+    'panel-heading' => 'There are :number related casualties in the system.',
+    'search'        => 'Search casualty by name',
+    'member-name'   => 'Name',
+    'death-date'    => 'Death date',
+    'birth-date'    => 'Birth date',
+    'death-reason'  => 'Cause of death',
+    'service-no'    => 'Service number'
+];
